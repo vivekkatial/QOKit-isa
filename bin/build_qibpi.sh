0684a55bc02b809a1c4e6edb7a12d64162d9498e
Vivek Katial <vivekkatial@gmail.com>
2024-05-15 20:09:12 +1000

    feat: Adding 50 jobs

--- bin/build_qibpi.sh
+++ bin/build_qibpi.sh
@@ -6,11 +6,14 @@ declare -a graph_types=("Nearly Complete BiPartite" "Uniform Random" "Power Law 
 # Define the array of weight types including None
 declare -a weight_types=("uniform" "uniform_plus" "normal" "exponential" "log-normal" "cauchy" "None")
 
-# Loop over each graph type and weight type
-for graph_type in "${graph_types[@]}"; do
-    for weight_type in "${weight_types[@]}"; do
-        # Submit the Slurm job for each combination with logging
-        echo "Submitting job for graph type: $graph_type, weight type: $weight_type"
-        sbatch --output=logs/%x-%j-%N.out bin/build_qibpi.slurm "$graph_type" "$weight_type"
+# Repeat the entire process 49 times
+for i in $(seq 1 49); do
+    # Loop over each graph type and weight type
+    for graph_type in "${graph_types[@]}"; do
+        for weight_type in "${weight_types[@]}"; do
+            # Submit the Slurm job for each combination with logging
+            echo "Submitting job for graph type: $graph_type, weight type: $weight_type, iteration: $i"
+            sbatch --output=logs/%x-%j-%N.out bin/build_qibpi.slurm "$graph_type" "$weight_type"
+        done
     done
 done
