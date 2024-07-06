c84f4e13dad0704722ba02880c149061d4ba5f8d
Vivek Katial <vivekkatial@gmail.com>
2024-07-06 16:10:27 +1000

    feat: Adding spartan runs

---
+++ bin/run_evolved_maxcut_instances.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define the file path to the evolved graph types
+evolved_inst_directory="spartan-ready-instances"
+
+# Loop over each node count
+for file in "$evolved_inst_directory"/*; do
+    # Submit the Slurm job for each combination with logging
+    echo "Run $i: Submitting job for file: $file"
+    # sbatch --output=logs/run-$(basename "$file")-$i-%x-%j-%N.out bin/run_evolved_instance.slurm "$file"
+done
