858a9e9320b7766200255a9e78745df20f802382
Vivek Katial <vivekkatial@gmail.com>
2024-08-28 22:08:33 +1000

    feat: Adding SLURM jobs

---
+++ bin/run_instances_n_layers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define the file path to the evolved graph types
+inst_directory="data/isa-instances"
+
+# Loop over each node count
+for file in "$evolved_inst_directory"/*; do
+    # Submit the Slurm job for each combination with logging
+    echo "Run $i: Submitting job for file: $file"
+    # sbatch --output=logs/run-$file-%x-%j-%N.out bin/run_evolved_instance.slurm "$file"
+done
