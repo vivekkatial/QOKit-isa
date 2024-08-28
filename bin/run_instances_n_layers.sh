7589ca40806a4b941dff167728b166ad3ed1f5b5
Vivek Katial <vivekkatial@gmail.com>
2024-08-28 22:10:01 +1000

    fix: Change inst path

--- bin/run_instances_n_layers.sh
+++ bin/run_instances_n_layers.sh
@@ -4,7 +4,7 @@
 inst_directory="data/isa-instances"
 
 # Loop over each node count
-for file in "$evolved_inst_directory"/*; do
+for file in "$inst_directory"/*; do
     # Submit the Slurm job for each combination with logging
     echo "Run $i: Submitting job for file: $file"
     # sbatch --output=logs/run-$file-%x-%j-%N.out bin/run_evolved_instance.slurm "$file"
