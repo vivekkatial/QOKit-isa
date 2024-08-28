8d80d2aa363948265b85b500ef87e7e17b2802f6
Vivek Katial <vivekkatial@gmail.com>
2024-08-28 22:11:02 +1000

    chore: Clean up

--- bin/run_instances_n_layers.sh
+++ bin/run_instances_n_layers.sh
@@ -5,7 +5,11 @@ inst_directory="data/isa-instances"
 
 # Loop over each node count
 for file in "$inst_directory"/*; do
+    # Extract the instance name from the file path
+    instance=$(basename "$file")
+    # Format the logfile name with the instance
+    logfile="logs/run-$instance-%x-%j-%N.out"
     # Submit the Slurm job for each combination with logging
     echo "Run $i: Submitting job for file: $file"
-    # sbatch --output=logs/run-$file-%x-%j-%N.out bin/run_evolved_instance.slurm "$file"
+    # sbatch --output="$logfile" bin/run_evolved_instance.slurm "$file"
 done
